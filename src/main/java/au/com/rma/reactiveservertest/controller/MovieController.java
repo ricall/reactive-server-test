e321c400b6a27c11c7d4240147613a842bee5729
Richard Allwood <richard.m.allwood@gmail.com>
2020-12-24 15:20:39 +1000

    Added MovieEvents to controller

--- src/main/java/au/com/rma/reactiveservertest/controller/MovieController.java
+++ src/main/java/au/com/rma/reactiveservertest/controller/MovieController.java
@@ -1,11 +1,15 @@
 package au.com.rma.reactiveservertest.controller;
 
 import au.com.rma.reactiveservertest.entity.Movie;
+import au.com.rma.reactiveservertest.entity.MovieEvent;
 import au.com.rma.reactiveservertest.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.awt.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +31,11 @@ public class MovieController {
     @PostMapping("")
     public Mono<Movie> addMovie(@RequestBody Movie movie) {
         return service.addMovie(movie);
+    }
+
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<MovieEvent> getMovieEvents(@PathVariable("id") String id) {
+        return service.findMovieEventsById(id);
     }
 
 }
--- src/main/java/au/com/rma/reactiveservertest/service/MovieService.java
+++ src/main/java/au/com/rma/reactiveservertest/service/MovieService.java
@@ -1,11 +1,17 @@
 package au.com.rma.reactiveservertest.service;
 
 import au.com.rma.reactiveservertest.entity.Movie;
+import au.com.rma.reactiveservertest.entity.MovieEvent;
 import au.com.rma.reactiveservertest.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -25,4 +31,28 @@ public class MovieService {
         return repository.save(movie);
     }
 
+    public Flux<MovieEvent> findMovieEventsById(String id) {
+        return findMovieById(id)
+                .flatMapMany(this::movieEvents);
+    }
+
+    private Flux<MovieEvent> movieEvents(Movie movie) {
+        return Flux.interval(Duration.ofMillis(500))
+                    .flatMap(i -> Flux.create(sink -> sink.next(MovieEvent.builder()
+                            .movie(movie)
+                            .started(LocalDateTime.now())
+                            .username(randomUsername())
+                            .build())));
+    }
+
+    private static String[] DEFAULT_USERNAMES = {
+            "user1",
+            "richard",
+            "guest",
+            "admin"
+    };
+
+    private String randomUsername() {
+        return DEFAULT_USERNAMES[new Random().nextInt(DEFAULT_USERNAMES.length)];
+    }
 }
